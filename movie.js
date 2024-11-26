43b1c8835042712b02ce2b2f13bfd5e4f77564c6
Fuad Jamal <jamalfuad34@gmail.com>
2024-11-26 18:33:20 +0200

    adding forms

--- movie.js
+++ movie.js
@@ -1,3 +1,7 @@
-<script>
-    document.getElementById("start").style.color="green"
-</script>
+document.getElementById("start").style.color="green"
+let b = document.forms["post"];
+let text = '';
+for(let i=0; i<b.length; i++) {
+    text+=b.elements[i].name +'<br>';
+}
+document.getElementById("neto").innerHTML= text;
