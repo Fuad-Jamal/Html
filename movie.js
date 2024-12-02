af5db5ffc5b5290e832fc4e4b8840004272569ce
Fuad Jamal <jamalfuad34@gmail.com>
2024-12-02 15:18:08 +0200

    still improving

--- movie.js
+++ movie.js
@@ -5,15 +5,42 @@ for(let i=0; i<b.length; i++) {
     text+=b.elements[i].name +'<br>';
 }
 
-let btn = document.querySelector('button');
+let btn = document.querySelector('.click-button');
 let message = document.querySelector('h4')
 btn.addEventListener('click', () =>{
  btn.innerText = 'Miss Me !!';
 });
-document.getElementById("eme").style.color="gold" 
+document.getElementById("eme").style.color="indigo" 
 
 document.getElementById("neto").innerHTML = "Bombo Clat";
 
 /*const x = document.getElementById("eme");
 const y = document.getElementsByTagName("h3");
-document.getElementById("eme").innerHTML = 'In the beninging ' + y[0].innerHTML;
+document.getElementById("eme").innerHTML = 'In the beninging ' + y[0].innerHTML;*/
+/*function changeText(){
+    document.getElementById("neto").textContent = "youhuu!!"
+}
+function changeBackgroundColor() {
+    document.getElementById("atmost").style.backgroundColor = "yellow"
+    
+}*/
+/*function addItem() {
+    const lise= document.createElement("li")
+    lise.textContent="a one"
+    document.getElementById("why").appendChild(lise)
+}*/
+/*function showVisibility() {
+    let can = document.getElementById("container")
+    can.style.visibility='hidden'
+}
+function hideVisibility() {
+    let nox = document.getElementById("container")
+    nox.style.visibility='visible'
+}*/
+function changeFontSize() {
+    document.getElementById("ibaze").style.fontSize = "70%"
+}
+document.getElementById("date").onclick = displayDate;
+function displayDate(){
+    document.getElementById("show").innerHTML= Date();
+}
